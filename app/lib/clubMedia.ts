f6b46a123f94f3e36099f78b1b45d6cc51b646ac
Wickey23 <Sameerk0723@gmail.com>
2026-08-08 13:25:55 -0400

    Add official club media sources

---
+++ app/lib/clubMedia.ts
@@ -0,0 +1,15 @@
+export const clubMedia = {
+  conversation: [
+    "https://media.licdn.com/dms/image/v2/D4E22AQE-CAE0ToYQGA/feedshare-shrink_800/B4EZoydz1sKgAg-/0/1761783269826?e=2147483647&t=qfYhZSfYCH0TftzR6PnuiVxipSH0gBeKxGrMqrlvEbk&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQHzmvopsJFE7A/feedshare-image-high-res/B4EZoydz2EKMAo-/0/1761783271995?e=2147483647&t=PviuMkp4kJT9ojsU9eyO8mQlErU0kE4cUcYGFyimtSo&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQE1T4nxsCCS0Q/feedshare-shrink_800/B4EZoydz2LIQAg-/0/1761783271435?e=2147483647&t=DX9D_kbraoinqC1UcDtVf192aXW9FbUN5jD8oEofcgg&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQFfrR_tbmeWyA/feedshare-image-high-res/B4EZoydz2JKsAo-/0/1761783271914?e=2147483647&t=M53T20IIKW1x-8eFJqNHMXNl8akvFzsxwZC_B4qJpDA&v=beta"
+  ],
+  board: [
+    "https://media.licdn.com/dms/image/v2/D4E22AQGZ69hEvWYZeA/feedshare-shrink_1280/B4EZoyYTH.HcAs-/0/1761781822493?e=2147483647&t=qEmDMWys50NbjcgJ0Tk2ZqzD6X7UBK7s8m6HuKIpwFY&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQGRn8MtaAVeCg/feedshare-image-high-res/B4EZoyYTH9HEAo-/0/1761781822640?e=2147483647&t=OmihnD_FhxxL5fza1ZMABb_ot-5ptNBZb29lmpQ9wr8&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQGeRqLkmMxouA/feedshare-image-high-res/B4EZoyYTHaIUAs-/0/1761781822668?e=2147483647&t=LsT3OCsE12h2UstkQ-41x-maUVT1mPilG7YlFQ4FoJg&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQEpoP0Q0l9DcQ/feedshare-shrink_1280/B4EZoyYTHyIoAs-/0/1761781822604?e=2147483647&t=eyk-sZYhsGTrkRERcetLsaNRgTNsvPfaY0UK0q51Rv4&v=beta",
+    "https://media.licdn.com/dms/image/v2/D4E22AQGgrIrJ0BvtwQ/feedshare-image-high-res/B4EZoyYTHxKMAo-/0/1761781822674?e=2147483647&t=QuqNHDSx-dB9Uw7lX4zwJc0PNkqp3jf1oNQwk8RSnMs&v=beta"
+  ]
+} as const;
