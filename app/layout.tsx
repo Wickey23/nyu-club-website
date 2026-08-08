fba0bb4e0e2eccccfa19e5fbc81c15cab3123d4f
Wickey23 <Sameerk0723@gmail.com>
2026-08-08 13:44:50 -0400

    Load sourced gallery styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import "./gallery-extra.css";
 
 const sans = DM_Sans({
   subsets: ["latin"],
