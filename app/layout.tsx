f21a7a173efe056d12e4ba527e4364884266ff57
Wickey23 <Sameerk0723@gmail.com>
2026-08-15 13:59:29 -0400

    Load team year filter styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./phase2.css";
 import "./page-polish.css";
 import "./instagram.css";
 import "./public-pages-editor.css";
+import "./team-filter.css";
 
 const sans=DM_Sans({subsets:["latin"],variable:"--font-sans",display:"swap"});
 const display=Playfair_Display({subsets:["latin"],variable:"--font-display",display:"swap"});
